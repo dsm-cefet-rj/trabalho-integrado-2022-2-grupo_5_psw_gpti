73d37d71c8f653021f02729ccc8cfafa6fdb6dbc
Bernardo Macedo <macedobernardo091@gmail.com>
2022-11-14 17:37:17 -0300

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,6 @@ export default function Navbar() {
           setIsNavExpanded(!isNavExpanded)
         }}
       >
-          {/* icon from heroicons.com */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
