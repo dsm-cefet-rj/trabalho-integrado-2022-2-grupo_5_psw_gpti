4f70a629f907b0667d3250f8739ad58e5fde0932
Rodrigo Parracho <52930258+RodrigoKasama@users.noreply.github.com>
2022-11-16 11:11:39 -0300

    Update Member Default

--- frontend/src/components/Equipe/Functions/ListMembers.js
+++ frontend/src/components/Equipe/Functions/ListMembers.js
@@ -1,32 +1,38 @@
 
 function Player(props) {
 	return (
-		<div className="PlayerDiv">
-			<div className="Cabecalho">
-				<img src="../../public/defaultPlaye.png" alt="img" /> <u>{props.player.name} </u>
-				<button className="editPlayer" type="" action="">Editar Dados</button>
-				<button className="removePlayer" type="" action="">Remover da Equipe</button>
+		<>
+			<div className="PlayerDiv">
+				<div className="Cabecalho">
+					<img src="public/defaultPlayer.png" alt="img" width="125px" height="125px"/> <u>{props.player.name}</u>
+					<button className="editPlayer" type="" action="">Editar Dados</button>
+					<button className="removePlayer" type="" action="">Remover da Equipe</button>
+				</div>
+				<label>Nome:</label>{props.player.sal}<br />
+				<label>Status:</label>{props.player.status}<br />
+				<button className="AddTask" type="" action="">Adicionar Meta</button>
 			</div>
-			<label>Nome:</label>{props.player.sal}<br />
-			<label>Status:</label>{props.player.status}<br />
-			<button className="AddTask" type="" action="">Adicionar Meta</button>
-		</div>
+			<br />
+		</>
 	);
 }
 
 
-// function AddMember() {
-// 	return (
-
-// 	);
-// }
+function AddMember() {
+	return (
+		<div className="AddPlayerDiv">
+			<label>Novo Membro</label><br/>
+			<button>Adiconar Membro</button>
+		</div>
+	);
+}
 
 export default function ListMembers(props) {
 	return (
 		<>
 			<div className="ListMembers">
-				{props.membros.map((player) => (<Player player={player} />))}
-
+				{props.membros.map((player) => (<Player player={player} key={player} />))}
+				<AddMember/>
 			</div>
 		</>
 	);
