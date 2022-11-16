7a1c47e5636ce42be8432bd5d604e567877ba40c
Rodrigo Parracho <52930258+RodrigoKasama@users.noreply.github.com>
2022-11-16 17:58:49 -0300

    Equipes Funcional mas nao funcionando

--- frontend/src/components/Equipe/Functions/ListMembers.js
+++ frontend/src/components/Equipe/Functions/ListMembers.js
@@ -1,44 +1,48 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
 
-export default class ListMembers extends Component {
+export default function ListMembers(props) {
 
-	constructor(props){
-		super(props);
-		this.state = {
-			membros: [
-				{ name: "Rodrigo", sal: 7000, status: "Disponivel", num: 1 },
-				{ name: "Bernado", sal: 7000, status: "Indisponivel", num: 2 },
-				{ name: "Vinicius", sal: 7000, status: "Indisponivel", num: 3 }
-			]
-		};
+	const [membros, setMembros] = useState([
+		{ name: "Rodrigo", sal: 7000, status: "Disponivel", id: 1 },
+		{ name: "Bernado", sal: 7000, status: "Indisponivel", id: 2 },
+		{ name: "Vinicius", sal: 7000, status: "Indisponivel", id: 3 }
+	]
+	);
+
+	function handleOnClickRemoveMember(param) {
+		setMembros(membros.filter((member) => member.name !== param))
 	}
-	
-	render() {
-		return (
-			<>
-				<h3>Lista de Membros</h3>
-				<div className="MembersList">
-					<Members membros={this.state.membros} />
-					<AddMember />
-				</div>
-			</>
-		);
+
+
+	function handleOnClickAddMember() {
+		setMembros(membros.concat({ name: "Diogo", Salario: 27000, status: "Indisponível", id: 4 }));
 	}
+
+	return (
+		<>
+			<h3>Lista de Membros</h3>
+			<div className="MembersList">
+				<Members membros={membros} onClickRemoveMember={handleOnClickRemoveMember} />
+			</div>
+			<AddMember onClickAddMember={handleOnClickAddMember} />
+		</>
+	);
 }
+
 
 function Player(props) {
 	return (
 		<>
 			<div className="PlayerDiv">
 				<div className="Cabecalho">
-					<img src="../../../../public/defaultPlayer.png" alt="img" width="125px" height="125px"/> <b><u>{props.player.name}</u></b>
-					<button className="editPlayer" type="" action="">Editar Dados</button>
-					<button className="removePlayer" type="" action="">Remover da Equipe</button>
+					<img src="../../../../public/defaultPlayer.png" alt="img" width="125px" height="125px" /> <b><u>{props.player.name}</u></b>
+					<button className="editPlayer" onClick={()=>{}}>Editar Dados</button>
+					<button className="removePlayer" onClick={() => props.onClickRemoveMember(props.player.name)}>Remover da Equipe</button>
 				</div>
 				<label>Nome:</label>{props.player.sal}<br />
 				<label>Status:</label>{props.player.status}<br />
-				<button className="AddTask" type="" action="">Adicionar Meta</button>
+				<button className="AddTask" onClick={()=>{}}>Adicionar Meta</button>
 			</div>
 			<br />
 		</>
@@ -47,19 +51,21 @@ function Player(props) {
 
 function Members(props) {
 	return (
-			<div className="ListMembers">
-			{props.membros.map((player) => (<Player player={player} key={player.num} />))}
-				{/* <AddMember/> */}
-			</div>
-	);
-}
-
-function AddMember() {
-	return (
-		<div className="AddPlayerDiv">
-			<label>Novo Membro</label><br/>
-			<button onClick={()=>alert("Membro criado")}>Adiconar Membro</button>
+		<div className="ListMembers">
+			{props.membros.map((player) => (<Player player={player} key={player.id} onClickRemoveMember={props.onClickRemoveMember} />))}
+			{/* <AddMember/> */}
 		</div>
 	);
 }
+
+function AddMember(props) {
+	return (
+		<div className="AddPlayerDiv">
+			<label>Novo Membro</label><br />
+			<button onClick={() => props.onClickAddMember()}>Adiconar Membro</button>
+		</div>
+	);
+}
+
+
 
